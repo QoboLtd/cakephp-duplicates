d6ca320ab12959d77697f93a6b63bf05290b8cea
George Constantinou <georgec20001@gmail.com>
2018-08-08 13:27:47 +0300

    Unit tests for rule filters (task #6718)

---
+++ tests/TestCase/Filter/ExactFilterTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace Qobo\Duplicates\Filter;
+
+use Cake\TestSuite\TestCase;
+use Qobo\Duplicates\Filter\ExactFilter;
+
+class ExactFilterTest extends TestCase
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->instance = new ExactFilter(['field' => 'foo']);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function tearDown()
+    {
+        unset($this->instance);
+
+        parent::tearDown();
+    }
+
+    public function testGetValue()
+    {
+        $this->assertEquals('foo', $this->instance->getValue());
+    }
+}
