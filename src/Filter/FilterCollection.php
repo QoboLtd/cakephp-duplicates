fa0b98506239a6be308e3db4192d9bfb106d180d
George Constantinou <georgec20001@gmail.com>
2018-08-09 13:13:05 +0300

    Filter Collection class (task #6718)
    Added filter collection class that implements `IteratorAggregate`. This class is responsible for storing `\Qobo\Duplicates\Filter\FilterInterface` objects.

---
+++ src/Filter/FilterCollection.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Qobo\Duplicates\Filter;
+
+use ArrayIterator;
+use IteratorAggregate;
+
+/**
+ * This collection class is responsible for storing \Qobo\Duplicates\Filter\FilterInterface objects.
+ */
+final class FilterCollection implements IteratorAggregate
+{
+    /**
+     * Filter instances list.
+     *
+     * @var array
+     */
+    private $filters;
+
+    /**
+     * Constructor method.
+     *
+     * @param array $filters List of filter instances
+     */
+    public function __construct(FilterInterface ...$filters)
+    {
+        $this->filters = $filters;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function getIterator()
+    {
+        return new ArrayIterator($this->filters);
+    }
+}
