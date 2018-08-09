deecb0d013d3bbf402c286f2d1946fccbb19a533
George Constantinou <georgec20001@gmail.com>
2018-08-09 13:15:36 +0300

    Rule class adjustments (task #6718)
    Filters instantiation has been removed from Rule class. Rule class now requires `\Qobo\Duplicates\Filter\FilterCollection` during instantiation.

--- src/Rule.php
+++ src/Rule.php
@@ -2,6 +2,7 @@
 namespace Qobo\Duplicates;
 
 use InvalidArgumentException;
+use Qobo\Duplicates\Filter\FilterCollection;
 use Qobo\Duplicates\Filter\FilterInterface;
 use RuntimeException;
 
@@ -28,42 +29,10 @@ final class Rule implements RuleInterface
      * Constructor method.
      *
      * @param string $name Rule name
-     * @param array $config Rule configuration
+     * @param \Qobo\Duplicates\Filter\FilterCollection $filters Filters collection
      * @return void
      */
-    public function __construct($name, array $config)
-    {
-        $this->validateName($name);
-
-        $this->name = $name;
-
-        foreach ($config as $item) {
-            $this->validateFilter($item);
-
-            $className = 'Qobo\\Duplicates\\Filter\\' . ucfirst($item['filter']) . 'Filter';
-            if (! class_exists($className)) {
-                throw new RuntimeException(sprintf('Filter class "%s" does not exist', $className));
-            }
-
-            $filter = new $className($item);
-            if (! $filter instanceof FilterInterface) {
-                throw new RuntimeException(
-                    sprintf('Class "%s" must implement "%s" interface', $className, FilterInterface::class)
-                );
-            }
-
-            array_push($this->filters, $filter);
-        }
-    }
-
-    /**
-     * Rule name validator.
-     *
-     * @param string $name Validator name
-     * @return void
-     * @throws \InvalidArgumentException when name variable is not a string or is empty
-     */
-    private function validateName($name)
+    public function __construct($name, FilterCollection $filters)
     {
         if (! is_string($name)) {
             throw new InvalidArgumentException('Rule name must be a string');
@@ -72,28 +41,9 @@ final class Rule implements RuleInterface
         if ('' === trim($name)) {
             throw new InvalidArgumentException('Rule name is required');
         }
-    }
 
-    /**
-     * Rule filter validator.
-     *
-     * @param array $config Filter configuration
-     * @return void
-     * @throws \InvalidArgumentException when filter key is not defined or filter value is not a string or is empty
-     */
-    private function validateFilter(array $config)
-    {
-        if (! isset($config['filter'])) {
-            throw new InvalidArgumentException('Rule filter name is required');
-        }
-
-        if (! is_string($config['filter'])) {
-            throw new InvalidArgumentException('Rule filter name must be a string');
-        }
-
-        if ('' === trim($config['filter'])) {
-            throw new InvalidArgumentException('Rule filter name is required');
-        }
+        $this->name = $name;
+        $this->filters = $filters;
     }
 
     /**
