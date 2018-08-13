c7937318be061f0cb429f3fe8e0cdaf9e7ead8cb
George Constantinou <georgec20001@gmail.com>
2018-08-13 17:48:56 +0300

    Extended unit tests for Duplicates table class (task #6718)

--- tests/TestCase/Model/Table/DuplicatesTableTest.php
+++ tests/TestCase/Model/Table/DuplicatesTableTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace Qobo\Duplicates\Test\TestCase\Model\Table;
 
+use Cake\Core\Configure;
 use Cake\Datasource\EntityInterface;
+use Cake\Datasource\ResultSetInterface;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
@@ -15,6 +17,7 @@ class DuplicatesTableTest extends TestCase
 {
     public $fixtures = [
         'plugin.CakeDC/Users.users',
+        'plugin.Qobo/Duplicates.articles',
         'plugin.Qobo/Duplicates.duplicates'
     ];
 
@@ -58,9 +61,9 @@ class DuplicatesTableTest extends TestCase
     {
         $this->assertInstanceOf(DuplicatesTable::class, $this->Duplicates);
 
-        $this->assertEquals('qobo_duplicates', $this->Duplicates->getTable());
-        $this->assertEquals('id', $this->Duplicates->getPrimaryKey());
-        $this->assertEquals('id', $this->Duplicates->getDisplayField());
+        $this->assertSame('qobo_duplicates', $this->Duplicates->getTable());
+        $this->assertSame('id', $this->Duplicates->getPrimaryKey());
+        $this->assertSame('id', $this->Duplicates->getDisplayField());
 
         $this->assertTrue($this->Duplicates->hasBehavior('Timestamp'));
 
@@ -102,5 +105,158 @@ class DuplicatesTableTest extends TestCase
 
         $this->assertInstanceOf(EntityInterface::class, $this->Duplicates->save($entity));
         $this->assertEmpty(array_diff($data, $entity->toArray()));
+    }
+
+    public function testMapDuplicates()
+    {
+        // overwrite Utils plugin configuration
+        Configure::write('CsvMigrations.modules.path', Configure::read('Duplicates.path'));
+
+        $this->assertSame([], $this->Duplicates->mapDuplicates());
+    }
+
+    public function testFetchByModelAndRule()
+    {
+        $expected = [
+            'pagination' => ['count' => 1],
+            'data' => [
+                ['id' => '00000000-0000-0000-0000-000000000002', 'value' => 'Second Article', 'count' => 1]
+            ]
+        ];
+
+        $this->assertSame($expected, $this->Duplicates->fetchByModelAndRule('Articles', 'byTitle', []));
+    }
+
+    public function testFetchByModelAndRuleWithOptions()
+    {
+        $options = ['page' => 0, 'size' => 1];
+        $expected = [
+            'pagination' => ['count' => 1],
+            'data' => [
+                ['id' => '00000000-0000-0000-0000-000000000002', 'value' => 'Second Article', 'count' => 1]
+            ]
+        ];
+
+        $this->assertSame($expected, $this->Duplicates->fetchByModelAndRule('Articles', 'byTitle', $options));
+
+        $options = ['page' => 1, 'size' => 1];
+        $expected = [
+            'pagination' => ['count' => 1],
+            'data' => [] // page 1 has no data
+        ];
+
+        $this->assertSame($expected, $this->Duplicates->fetchByModelAndRule('Articles', 'byTitle', $options));
+    }
+
+    public function testFetchByOriginalIDAndRule()
+    {
+        $result = $this->Duplicates->fetchByOriginalIDAndRule('00000000-0000-0000-0000-000000000002', 'byTitle');
+
+        $this->assertInstanceOf(EntityInterface::class, $result['original']);
+        $this->assertEquals(
+            TableRegistry::getTableLocator()
+                ->get('Articles')
+                ->get('00000000-0000-0000-0000-000000000002'),
+            $result['original']
+        );
+
+        $this->assertInstanceOf(ResultSetInterface::class, $result['duplicates']);
+        $this->assertEquals(
+            TableRegistry::getTableLocator()
+                ->get('Articles')
+                ->get('00000000-0000-0000-0000-000000000003'),
+            $result['duplicates']->first()
+        );
+
+        $this->assertSame(['id', 'title', 'excerpt', 'body', 'created', 'modified'], $result['fields']);
+
+        $this->assertSame([], $result['virtualFields']);
+    }
+
+    public function testFetchByOriginalIDAndRuleWithInvalidID()
+    {
+        $result = $this->Duplicates->fetchByOriginalIDAndRule('00000000-0000-0000-0000-000000000404', 'byTitle');
+
+        $this->assertSame([], $result);
+    }
+
+    public function testDeleteDuplicates()
+    {
+        $ids = ['00000000-0000-0000-0000-000000000002'];
+
+        $this->assertTrue($this->Duplicates->deleteDuplicates('Articles', $ids));
+
+        $query = Tableregistry::getTableLocator()
+            ->get('Articles')
+            ->find('all')
+            ->where(['id' => $ids[0]]);
+        $this->assertTrue($query->isEmpty());
+
+        $query = $this->Duplicates->find('all')
+            ->where(['id' => '00000000-0000-0000-0000-000000000001']);
+        $this->assertTrue($query->isEmpty());
+    }
+
+    public function testDeleteDuplicatesWithInvalidID()
+    {
+        // get duplcicates count
+        $count = $this->Duplicates->find('all')->count();
+        $ids = [
+            '00000000-0000-0000-0000-000000000001' // invalid duplicate ID
+        ];
+
+        $this->assertFalse($this->Duplicates->deleteDuplicates('Articles', $ids));
+
+        // invalid duplicate ID
+        $query = TableRegistry::getTableLocator()
+            ->get('Articles')
+            ->find('all')
+            ->where(['id' => $ids[0]]);
+        $this->assertFalse($query->isEmpty());
+
+        // duplicate records were not affected
+        $this->assertSame($count, $this->Duplicates->find('all')->count());
+    }
+
+    public function testFalsePositiveByRuleAndIDs()
+    {
+        $ids = ['00000000-0000-0000-0000-000000000003'];
+
+        $this->assertTrue($this->Duplicates->falsePositiveByRuleAndIDs('byTitle', $ids));
+
+        $entity = $this->Duplicates->get('00000000-0000-0000-0000-000000000001');
+        $this->assertSame('processed', $entity->get('status'));
+    }
+
+    public function testFalsePositiveByRuleAndIDsWithInvalidID()
+    {
+        $ids = ['00000000-0000-0000-0000-000000000404'];
+        $resultSet = $this->Duplicates->find()->all();
+
+        $this->assertFalse($this->Duplicates->falsePositiveByRuleAndIDs('byTitle', $ids));
+        $this->assertEquals($resultSet, $this->Duplicates->find()->all());
+    }
+
+    public function testMergeDuplicates()
+    {
+        $id = '00000000-0000-0000-0000-000000000002';
+        $data = ['excerpt' => 'Third'];
+
+        $this->assertTrue($this->Duplicates->mergeDuplicates('Articles', $id, $data));
+        $this->assertSame(
+            $data['excerpt'],
+            TableRegistry::getTableLocator()
+                ->get('Articles')
+                ->get($id)
+                ->get('excerpt')
+        );
+    }
+
+    public function testMergeDuplicatesWithInvalidID()
+    {
+        $id = '00000000-0000-0000-0000-000000000404';
+        $data = ['excerpt' => 'Third'];
+
+        $this->assertFalse($this->Duplicates->mergeDuplicates('Articles', $id, $data));
     }
 }
